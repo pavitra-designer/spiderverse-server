32adc054b4983dc991a343ac3102b1aaeb4a2ead
pavitra-designer <pavitrabhardwaj2@gmail.com>
2026-07-13 21:35:40 +0530

    l

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://spiderverse-eosin.vercel.app/",
+    "https://spiderverse-eosin.vercel.app",
 ];
 
 app.use(
